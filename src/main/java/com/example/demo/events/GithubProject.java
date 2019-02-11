6dd3aaac482d41679f94f112a0da580b9ff4791e
DOMAIN\starosta <starosta@cngroup.dk>
2019-02-11 10:54:49 +0100

    added code from presentation

---
+++ src/main/java/com/example/demo/events/GithubProject.java
@@ -0,0 +1,45 @@
+package com.example.demo.events;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+@Entity
+public class GithubProject implements Serializable {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String orgName;
+
+    @Column(unique = true)
+    private String repoName;
+
+    public GithubProject(){
+
+    }
+
+    public GithubProject(String orgName, String repoName) {
+        this.orgName = orgName;
+        this.repoName = repoName;
+    }
+
+    public Long getId() {return id; }
+
+    public String getOrgName() {return orgName; }
+
+    public void setOrgName(String orgName) {this.orgName = orgName; }
+
+    public String getRepoName() {return repoName; }
+
+    @Override
+    public String toString() {
+        return "GithubProject{" +
+                "id=" + id +
+                ", orgName=" + orgName + '\'' +
+                ", repoName=" + repoName + '\'' +
+                '}';
+    }
+}
