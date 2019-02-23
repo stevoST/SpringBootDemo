9994f488abba3c68ee06c8a8bc808e46cad8afe5
stevoST <stefan.starosta@gmail.com>
2019-02-23 22:19:38 +0100

    45 minute. spring application properties does not work

---
+++ src/main/java/com/example/demo/GithubProperties.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.constraints.Pattern;
+
+@ConfigurationProperties("github")
+@Validated
+public class GithubProperties {
+
+    /**
+     * Github API token ("user:sampletoken")
+     */
+    @Pattern(regexp = "\\w+:\\w+")
+    private String token;
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
--- src/main/java/com/example/demo/github/GithubClient.java
+++ src/main/java/com/example/demo/github/GithubClient.java
@@ -1,9 +1,20 @@
 package com.example.demo.github;
 
+import org.springframework.util.StringUtils;
+import com.example.demo.GithubProperties;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpRequest;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.ClientHttpRequestExecution;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.stereotype.Component;
+import org.springframework.util.Base64Utils;
 import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 @Component
 public class GithubClient {
@@ -12,11 +23,35 @@ public class GithubClient {
 
   private final RestTemplate restTemplate;
 
-  public GithubClient(RestTemplateBuilder builder) {
-    this.restTemplate = builder.build();
+  public GithubClient(RestTemplateBuilder builder, GithubProperties properties) {
+
+    this.restTemplate = builder
+            .additionalInterceptors(new GithubAppTokenInterceptor(properties.getToken()))
+            .build();
   }
 
   public ResponseEntity<RepositoryEvent[]> fetchEvents(String orgName, String repoName){
     return this.restTemplate.getForEntity(EVENT_ISSUES_URL, RepositoryEvent[].class, orgName, repoName);
   }
+
+  private static class GithubAppTokenInterceptor implements ClientHttpRequestInterceptor {
+
+    private final String token;
+
+    GithubAppTokenInterceptor(String token){
+      this.token = token;
+    }
+
+    @Override
+    public ClientHttpResponse intercept(HttpRequest httpRequest, byte[] bytes,
+                                        ClientHttpRequestExecution clientHttpRequestExecution) throws IOException {
+      if(StringUtils.hasText(this.token)) {
+        byte[] basicAuthValue = this.token.getBytes(StandardCharsets.UTF_8);
+        httpRequest.getHeaders().set(HttpHeaders.AUTHORIZATION,
+                "Basic " + Base64Utils.encodeToString(basicAuthValue));
+      }
+      return clientHttpRequestExecution.execute(httpRequest, bytes);
+    }
+  }
+
 }
